6030aa7dd63cb5feac56215eb134340fe292b6be
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-03-09 16:35:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
+# Specify your main script and options for py2app here
+APP = ['PasswordGenerator.py']  
+OPTIONS = {
+    'argv_emulation': True,
+    # Add any other py2app options you need here
+}
+
 setup(
     name="PasswordGenerator",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "pyperclip",
+        # Add other dependencies here
     ],
     entry_points={
         'gui_scripts': [
@@ -22,4 +30,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    options={'py2app': OPTIONS},  # py2app options
+    app=APP,  # py2app target
+    # Add any other necessary configuration for your package
 )
